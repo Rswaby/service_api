4055840d0258c0992be59abd696d5759bae2bda6
Rohan  Swaby <rohan_swaby51@yahoo.com>
2021-07-28 21:48:47 -0400

    db config setup  (#1)
    * db config

--- app.js
+++ app.js
@@ -3,11 +3,16 @@
 // load modules
 const express = require('express');
 const morgan = require('morgan');
+
+const { sequelize } = require('./models');
 // variable to enable global error logging
 const enableGlobalErrorLogging = process.env.ENABLE_GLOBAL_ERROR_LOGGING === 'true';
 
 // create the Express app
 const app = express();
+
+// request body JSON parsing
+app.use(express.json);
 
 // setup morgan which gives us http request logging
 app.use(morgan('dev'));
@@ -40,6 +45,16 @@ app.use((err, req, res, next) => {
 
 // set our port
 app.set('port', process.env.PORT || 5000);
+
+// Test db connection
+(async () => {
+  try {
+    await sequelize.authenticate();
+    console.log('Connection to DB has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+})();
 
 // start listening on our port
 const server = app.listen(app.get('port'), () => {
